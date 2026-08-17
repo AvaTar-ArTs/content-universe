bae3a96144af91481a623059f36986197495cc7d
AvaTar-ArTs <me@avatararts.org>
2026-08-16 22:46:03 -0400

    Add Content Universe package

---
+++ src/content_universe/__init__.py
@@ -0,0 +1,6 @@
+"""Content Universe: provenance-aware creative harvesting core."""
+
+from .catalog import Catalog, GenerationRecord, ResponseRecord
+
+__all__ = ["Catalog", "GenerationRecord", "ResponseRecord"]
+__version__ = "0.1.0"
